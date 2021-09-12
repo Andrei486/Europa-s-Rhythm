9df73cd519f1fd88cab823aa3b45c8ad47cc94de
Andrei486 <apopescu486@gmail.com>
2021-09-12 15:11:48 -0400

    Added placeholder scripts, prefabs for target line/miss line, and fixed physics layers.

---
+++ Europa's Rhythm/Assets/Scripts/NoteController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script should manage the note's movement (for now variable for speed)
+//as well as detect collisions, if the collision is with the miss detection line
+//(see layers) then destroy the object, show feedback (later) and update score
+
+public class NoteController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Europa's Rhythm/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script should manage the player's inputs. Placed on an arbitrary "player" object
+//in the scene, it checks every time the player presses one of the 4 note keys
+//to see whether a note of the correct type is overlapping the target line.
+//If there is such a note, the note should be destroyed and the score updated accordingly.
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
