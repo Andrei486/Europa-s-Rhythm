6dbf3ab3dab23d9a62421740f53e0e0c2d9c95b3
Josh-Sal <90338184+Josh-Sal@users.noreply.github.com>
2021-09-12 22:34:11 -0400

    score (#6)
    * score
    
    added a combo
    
    * Changed UI to use new Input System
    
    Co-authored-by: Andrei486 <apopescu486@gmail.com>

--- Europa's Rhythm/Assets/Scripts/ScoreManager.cs
+++ Europa's Rhythm/Assets/Scripts/ScoreManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //This script should manage the score system: other scripts will keep a reference
 //to this script (attched to the same object as Player.cs presumably).
@@ -11,26 +12,57 @@ public class ScoreManager : MonoBehaviour
 {
     public float Score {get; set;}
     private float comboBonus = 1.0f;
+    private int combo;
+    [SerializeField] private Text comboText;
+    //public int Combo {
+        //get {
+           // return combo;
+       // }
+        //set {
+           // combo = value;
+           // SetComboText(value);
+        //}
+    //}
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        combo = 0;
+        Score = 0f;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        comboText.text = combo.ToString();
     }
+
+    //private void SetComboText(int value)
+    //{
+       // comboText.text = combo.ToString();
+    //}
+
     public void OnClickMiss() {
         //update score & combo
+        combo = 0;
+        comboBonus = 1.0f;
+        Score -= 1;
     }
     public void OnPerfect() {
         //update score & combo
+        combo++;
+        comboBonus += 0.5f;
+        Score += 2f * comboBonus;
     }
     public void OnGood() {
         //update score & combo
+        combo++;
+        comboBonus += 0.25f;
+        Score += 1f * comboBonus;
     }
     public void OnNoteMiss() {
         //update score & combo
+        combo = 0;
+        comboBonus = 1.0f;
+        Score -= 4;
     }
 }
